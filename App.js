c842fa2b7ede4572ee0bbf58e39391deb890f7fc
glhrmarques <glhrmarques@hotmail.com>
2021-11-13 11:56:31 -0300

    main page

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, FlatList } from 'react-native';
+import Header from './components/Header';
+import AddTask from './components/AddTask';
+import Categories from './components/Categories';
+import TaskItem from './components/TaskItem';
+
 
 export default function App() {
+  
+  const [tasks, setTasks] = useState([
+    {text: 'task1', key: '1'},
+    {text: 'task2', key: '2'}
+
+  ]);
+
+  //Delete task
+  const deleteHandler = (key) => {
+    setTasks((deletedTasks) => {
+      return deletedTasks.filter(tasks => tasks.key != key);
+    });
+  } 
+
+//Add task
+const addHandler = (text) => {
+  setTasks((deletedTasks) => {
+    return [
+      {text: text, key: Math.random().toString()},
+      ...deletedTasks
+    ]
+  })
+}
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <StatusBar barStyle='light-content' />
+        <SafeAreaView style={{flex:0, backgroundColor: '#0081a7'}}/>
+        <SafeAreaView style={{flex:1, backgroundColor: '#fafafa'}}>
+
+          {/*Header component */}
+          <Header name={'Page Name'} />
+          
+          {/*Content in the screen*/}
+          <View style={styles.screenContent}>
+
+            {/*Submit Section*/}
+            <AddTask addHandler={addHandler}/>
+          
+            {/*Categories Carousel */}
+            <Categories />
+
+            <FlatList
+              data={tasks}
+              renderItem={({item}) => (
+                <TaskItem item={item} deleteHandler={deleteHandler} />
+              )}
+            />
+          </View>
+        </SafeAreaView>
+      </>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  
+  screenContent: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#fafafa',
+  }
+})
